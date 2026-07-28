539bdd5da9ae9bf8dbed71f380e1553ebfb36b0e
Kavin <immkavin.ofcl@gmail.com>
2026-07-28 07:38:25 +0530

    Remove insult API call from index.js
    Removed the insult API call and related data from the response.

--- index.js
+++ index.js
@@ -11,15 +11,9 @@ app.get("/", async (req, res) => {
   try {
     const response1 = await axios.get("https://meme-api.com/gimme");
     const result1 = response1.data;
-    const response2 = await axios.get(
-      "https://evilinsult.com/generate_insult.php?type=json"
-    );
-    const result2 = response2.data;
     res.render("index.ejs", {
       memeTitle: result1.title,
-      memeUrl: result1.preview.slice(-1),
-      insult: result2.insult,
-      creator: result2.createdby,
+      memeUrl: result1.preview.slice(-1)
     });
   } catch (error) {
     console.error(error.message);
